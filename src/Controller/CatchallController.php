35831423f30804feb613f17a00b8abcf9a9b5feb
Alexandre Waeles <axelandre42@yahoo.fr>
2018-04-03 21:24:55 +0000

    Added update management

---
+++ src/Controller/CatchallController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\TelegramCommand\StartCommand;
+use Ialert\TelegramBotBundle\Telegram\Command\CommandChain;
+use Ialert\TelegramBotBundle\Telegram\TelegramApi;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+    private $telegramApi;
+    private $commandChain;
+    private $doctrine;
+    
+    public function __construct(TelegramApi $telegramApi, CommandChain $commandChain, RegistryInterface $doctrine)
+    {
+        $this->telegramApi = $telegramApi;
+        $this->commandChain = $commandChain;
+        $this->doctrine = $doctrine;
+    }
+    
+    public function telegramUpdate(Request $request)
+    {
+        $this->commandChain->addCommand(new StartCommand($this->doctrine));
+        
+        return $this->telegramApi->handleUpdate($request);
+    }
+}
